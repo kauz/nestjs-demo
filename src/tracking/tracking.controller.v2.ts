cb80efc6403ffab350d218d663521bf9cf06cd91
Artyom Popov <ArtemPopov@v.stockx.com>
2021-10-05 19:23:47 +0300

    get back v2 for tracking controller

--- src/tracking/tracking.controller.v2.ts
+++ src/tracking/tracking.controller.v2.ts
@@ -2,7 +2,10 @@ import { Body, Controller, Put } from '@nestjs/common';
 import { UpdateTrackingV2Dto } from '@root/tracking/dtos/update-tracking-v2.dto';
 import { TrackingService } from './tracking.service';
 
-@Controller('shipments/tracking')
+@Controller({
+  version: '2',
+  path: 'shipments/tracking'
+})
 export class TrackingControllerV2 {
   constructor(private readonly trackingService: TrackingService) {}
 
